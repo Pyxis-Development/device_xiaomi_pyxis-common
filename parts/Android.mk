569401f7772aa4cb8de811f3f7509aef3f831093
TheScarastic <warabhishek@gmail.com>
2019-09-09 20:33:29 +0530

    pyxis,vela: Add DC dimming
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- parts/Android.mk
+++ parts/Android.mk
@@ -27,6 +27,9 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-appcompat \
     android-support-v14-preference
 
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    vendor.xiaomi.hardware.displayfeature-V1.0-java
+
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
