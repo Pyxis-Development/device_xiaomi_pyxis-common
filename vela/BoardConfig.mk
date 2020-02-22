3d1a124b21e3b44c9a6a0fc5dcca842e9687a512
Mashopy <eliasgheeraert@gmail.com>
2020-02-22 13:57:36 +0100

    vela: Add Meitu Camera hardware manifest

--- vela/BoardConfig.mk
+++ vela/BoardConfig.mk
@@ -13,6 +13,9 @@ TARGET_OTA_ASSERT_DEVICE := vela
 # Kernel
 TARGET_KERNEL_CONFIG := vela_defconfig
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(PLATFORM_PATH)/vela/manifest.xml
+
 # Inherit from the proprietary version
 -include vendor/meitu/vela/BoardConfigVendor.mk
 -include vendor/xiaomi/pyxis/BoardConfigVendor.mk
