580ec843afa91dffd561219c27916751a159a83a
coderzstas <coderz35@gmail.com>
2020-02-24 23:54:36 +0300

    pyxis: build gps from source. from LA.UM.8.8.r1-06800-SDM710.0

--- device-common.mk
+++ device-common.mk
@@ -129,8 +129,15 @@ PRODUCT_PACKAGES += \
     qcom.fmradio
     
 # GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.0-impl-qti \
+    android.hardware.gnss@2.0-service-qti \
+    libbatching \
+    libgeofencing \
+    libgnss
+
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps,$(TARGET_COPY_OUT_VENDOR)/etc)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/gps/etc/,$(TARGET_COPY_OUT_VENDOR)/etc)
     
 # Health
 PRODUCT_PACKAGES += \
